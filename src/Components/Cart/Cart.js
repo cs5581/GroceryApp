9dab5f552a6e8a780cc9818c953016c9bdeb1f30
Management Reports Team 3 <cs558@njit.edu>
2023-05-26 22:26:43 -0400

    Working on Checkout portion on Modal

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,16 +1,25 @@
 import React from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import createContextNow from "../../Store/Cart-Context";
 import CartItem from "./CartItem";
 import { Routes, Route, useNavigate } from "react-router-dom";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   const cartCTX = useContext(createContextNow);
   const totalAmount = `$${cartCTX.totalAmount.toFixed(2)}`;
   const hasItems = cartCTX.items.length > 0;
+  const [isReadyToCheckout, setIsReadyToCheckout] = useState(false);
+
+  /* <Routes>
+  <Route
+    path="../Pages/PlaceOrderScreen/"
+    element={<PlaceOrderScreen />}
+  ></Route>
+</Routes>*/
 
   const cartItemRemoveHandler = (id) => {
     cartCTX.removeItem(id);
@@ -18,9 +27,13 @@ const Cart = (props) => {
   const cartItemAddHandler = (item) => {
     cartCTX.addItem({ ...item, amount: 1 });
   };
-  const redirectionPlaceOrderHandler = () => {
-    navigate("../Pages/PlaceOrderScreen/");
+
+  const onOrderHandler = () => {
+    setIsReadyToCheckout(true);
   };
+  //const redirectionPlaceOrderHandler = () => {
+  //navigate("../Pages/PlaceOrderScreen/");
+  //};
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -37,6 +50,19 @@ const Cart = (props) => {
     </ul>
   );
 
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onDisengage}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={onOrderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
   return (
     <Modal onDisengage={props.onDisengage}>
       {cartItems}
@@ -44,25 +70,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onDisengage}>
-          Close
-        </button>
-        {hasItems && (
-          <button
-            className={classes.button}
-            onClick={redirectionPlaceOrderHandler}
-          >
-            Order
-          </button>
-        )}
-        <Routes>
-          <Route
-            path="../Pages/PlaceOrderScreen/"
-            element={<PlaceOrderScreen />}
-          ></Route>
-        </Routes>
-      </div>
+      {isReadyToCheckout && <Checkout onCancel={props.onDisengage} />}
+      {!isReadyToCheckout && modalActions}
     </Modal>
   );
 };
